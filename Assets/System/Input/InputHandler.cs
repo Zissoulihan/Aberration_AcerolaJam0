d3fc09324b56865d49db4266f2d81406504c02c0
Zissoulihan <85265810+Zissoulihan@users.noreply.github.com>
2024-02-29 12:17:35 -0700

    Setup input handling
    Added input handling for basic fps stuff since I'm probably making this a first person pov game. Set game event & SV icons but now I think the events should have the rhombus shape and the SVs the circle, but I'm too lazy to change them all again now reeee

--- Assets/System/Input/InputHandler.cs
+++ Assets/System/Input/InputHandler.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour
 {
-    
+    [Header("Move Input")]
+    [SerializeField] GameEventVector2 _evInputMove;
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        _evInputMove.TriggerEvent(context.ReadValue<Vector2>().normalized);
+    }
+
+    [Header("Interaction Input")]
+    [SerializeField] GameEventVoid _evInteractPress;
+    [SerializeField] GameEventVoid _evInteractRelease;
+    public void OnInteractInput(InputAction.CallbackContext context)
+    {
+        if (context.started) _evInteractPress.TriggerEvent();
+        if (context.canceled) _evInteractRelease.TriggerEvent();
+    }
+
+    [Header("Mouse Input")]
+    [SerializeField] SharedVariableVector2 _svInputMousePos;
+    public void OnUpdateMousePos(InputAction.CallbackContext context)
+    {
+        _svInputMousePos.Set(context.ReadValue<Vector2>());
+    }
+
+    [SerializeField] SharedVariableVector2 _svInputMouseDelta;
+    public void OnUpdateMouseDelta(InputAction.CallbackContext context)
+    {
+        _svInputMouseDelta.Set(context.ReadValue<Vector2>());
+    }
 }
