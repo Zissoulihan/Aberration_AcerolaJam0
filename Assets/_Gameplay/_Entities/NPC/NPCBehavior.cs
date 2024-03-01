402e060d95d4774d7250bee0831cee1dee313a72
Zissoulihan <85265810+Zissoulihan@users.noreply.github.com>
2024-03-01 15:13:11 -0700

    Add NPC hiding
    NPCs will move to the closest unoccupied hiding spot now when they see the player, after spending an amount of time frozen in fear.
    
    Probably will want something more organized for NPCBehavior soon. Probably a state machine, even though we don't enjoy those lol

--- Assets/_Gameplay/_Entities/NPC/NPCBehavior.cs
+++ Assets/_Gameplay/_Entities/NPC/NPCBehavior.cs
@@ -13,9 +13,11 @@ public class NPCBehavior : MonoBehaviour
     NPCMovement _move;
 
     bool _playerDetected;
+    bool _hidingSpotFound;
 
     float _timeLastPlayerCheck;
-
+    float _timeLastPlayerSeen;
+    
     private void Awake()
     {
         Init();
@@ -29,6 +31,7 @@ public class NPCBehavior : MonoBehaviour
     private void Update()
     {
         CheckForPlayer();
+        FindHidingSpot();
     }
 
     void CheckForPlayer()
@@ -40,6 +43,38 @@ public class NPCBehavior : MonoBehaviour
 
         print($"Oh sweet jesus fuck, run!");
         _playerDetected = true;
+        _timeLastPlayerSeen = Time.time;
+    }
+
+    void FindHidingSpot()
+    {
+        if (!_playerDetected) return;
+        if (_hidingSpotFound) return;
+        if (Time.time < _timeLastPlayerSeen + _durationShock) return;
+
+        if (NPCHidingSpot.HidingSpots == null || NPCHidingSpot.HidingSpots.Count == 0) {
+            //TODO: Panik
+            return;
+        }
+
+        float minDist = 999f;
+        NPCHidingSpot goalSpot = null;
+        foreach (var spot in NPCHidingSpot.HidingSpots) {
+            if (spot == null || spot.Claimed) continue;
+            float distToSpot = Vector3.Distance(_move.transform.position, spot.HidingPosition);
+            if (distToSpot < minDist) {
+                goalSpot = spot;
+                minDist = distToSpot;
+            }
+        }
+        if (goalSpot == null) {
+            print("Hiding spots searched but none could be found!");
+            return;
+        }
+
+        goalSpot.CallDibs();
+        _move.MoveToGoal(goalSpot.HidingPosition);
+        _hidingSpotFound = true;
     }
 
 }
--- Assets/_Level/POI/Hiding/NPCHidingSpot.cs
+++ Assets/_Level/POI/Hiding/NPCHidingSpot.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class NPCHidingSpot : MonoBehaviour
 {
     [SerializeField] Transform _hideSpot;
+
+    public static List<NPCHidingSpot> HidingSpots;
     public bool Claimed { get; private set; }
     public Vector3 HidingPosition => _hideSpot.position;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        if (HidingSpots == null) HidingSpots = new List<NPCHidingSpot>();
+        HidingSpots.Add(this);
+    }
 
     public void CallDibs()
     {
         if (Claimed) return;
 
         Claimed = true;
+        HidingSpots?.Remove(this);
     }
 }
