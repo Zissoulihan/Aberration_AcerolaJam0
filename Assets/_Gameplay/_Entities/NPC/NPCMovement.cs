e09c4e068af940cadda10bee0281923fac9f4e6e
Zissoulihan <85265810+Zissoulihan@users.noreply.github.com>
2024-03-04 14:56:39 -0700

    Add empty NPC behavior scripts
    The gf got a concussion last night, so not getting too much done today at all

--- Assets/_Gameplay/_Entities/NPC/NPCMovement.cs
+++ Assets/_Gameplay/_Entities/NPC/NPCMovement.cs
@@ -1,14 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class NPCMovement : MonoBehaviour
 {
-    
+    [SerializeField] float _distanceConsiderGoalReached;
+    [SerializeField] SharedVariableBool _svGamePaused;
+
+    public event Action OnReachGoal;
+
     NavMeshAgent _nav;
 
     public Vector3 GoalPos { get; private set; }
+
+    bool _movingToGoal;
+    bool _movePaused;
 
     private void Awake()
     {
@@ -24,5 +30,36 @@ public class NPCMovement : MonoBehaviour
     {
         GoalPos = goal;
         _nav.destination = goal;
+        _movingToGoal = true;
+    }
+
+    private void Update()
+    {
+        HandlePausing();
+        CheckMoveProgress();
+    }
+
+    void HandlePausing()
+    {
+        if (!_movingToGoal) return;
+        if (_svGamePaused.Value) {
+            if (_movePaused) return;
+            _nav.isStopped = true;
+            _movePaused = true;
+        }
+        else {
+            if (!_movePaused) return;
+            _nav.isStopped = false;
+            _movePaused = false;
+        }
+    }
+
+    void CheckMoveProgress()
+    {
+        if (!_movingToGoal) return;
+        if (!_nav.hasPath) return;
+        if (_nav.remainingDistance > _distanceConsiderGoalReached) return;
+        OnReachGoal?.Invoke();
+        _movingToGoal = false;
     }
 }
